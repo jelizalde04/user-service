48b324ad61d348c101cb0ce0f7720f41fd98c3d6
Alexis Mendoza <afmendozaf@uce.edu.ec>
2025-02-11 14:52:31 -0500

    refactor(models): 🔄 rename fields to camelCase and fix table name

--- user-delete-microservice/app/models/user.py
+++ user-delete-microservice/app/models/user.py
@@ -3,11 +3,11 @@ from app.services.db_service import Base
 import datetime
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "Users"  # Tabla con "U" mayúscula
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     password = Column(String)
     email = Column(String, unique=True, index=True)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+    createdAt = Column(DateTime, default=datetime.datetime.utcnow)  # Cambio de created_at a createdAt
+    updatedAt = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)  # updatedAt
