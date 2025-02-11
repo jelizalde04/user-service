8d451389c01e4831b5062454e2048051a869ce07
Alexis Mendoza <afmendozaf@uce.edu.ec>
2025-02-11 17:24:16 -0500

    feat(api): ✨ add update user endpoint

--- user-update-microservice/app/routes/update_routes.py
+++ user-update-microservice/app/routes/update_routes.py
@@ -1,11 +1,18 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel, Field
 from app.controllers.update_user_controller import update_user
 
 router = APIRouter()
 
+# Definir el esquema que FastAPI usará para validar los datos de entrada
+class UpdateUserRequest(BaseModel):
+    username: str = Field(None, example="Kafka")
+    email: str = Field(None, example="nuevo_correo@example.com")
+    password: str = Field(None, example="kafka123")
+
 @router.put("/users/{user_id}")
-def update_user_endpoint(user_id: int, username: str = None, email: str = None, password: str = None):
-    result = update_user(user_id, username, email, password)
+def update_user_endpoint(user_id: int, user_data: UpdateUserRequest):
+    result = update_user(user_id, user_data.username, user_data.email, user_data.password)
     if "error" in result:
         raise HTTPException(status_code=400, detail=result["error"])
     return result
