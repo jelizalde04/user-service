941805bdba1de943fa4dde1112c19e236bfbf504
Johana Elizalde <njelizalde@uce.edu.ec>
2025-02-11 14:28:10 -0500

    feat(models): 🏗️ add user model

---
+++ user-delete-microservice/app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from app.services.db_service import Base
+import datetime
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+    email = Column(String, unique=True, index=True)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
