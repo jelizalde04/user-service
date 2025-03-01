7f94079dbb9a12ae71599eadebffbf7e0f0e4771
Johana Elizalde <njelizalde@uce.edu.ec>
2025-03-01 14:24:11 -0500

    feat(db): 🛢️ add database connection logic

---
+++ list-users-microservice/db.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "database/sql"
+    "fmt"
+    "log"
+    "os"
+
+    _ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+    dbHost := os.Getenv("DB_HOST")
+    dbPort := os.Getenv("DB_PORT")
+    dbUser := os.Getenv("DB_USER")
+    dbPassword := os.Getenv("DB_PASSWORD")
+    dbName := os.Getenv("DB_NAME")
+
+    dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+        dbHost, dbPort, dbUser, dbPassword, dbName)
+
+    var err error
+    DB, err = sql.Open("postgres", dsn)
+    if err != nil {
+        log.Fatal("Error al conectar con la base de datos:", err)
+    }
+
+    if err = DB.Ping(); err != nil {
+        log.Fatal("No se pudo establecer la conexión con la base de datos:", err)
+    }
+
+    fmt.Println("Base de datos conectada exitosamente")
+}
